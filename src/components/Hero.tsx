0a1a4edfee016c08b164ad2f8477bc8ac522fabb
azuazu1313 <asnismarts7@gmail.com>
2025-03-15 19:52:01 +0100

    Add files via upload

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,20 +3,21 @@ import SearchForm from './SearchForm';
 
 const Hero = () => {
   return (
-    <div id="booking-form" className="relative h-[800px] md:h-auto md:min-h-screen">
+    <div id="booking-form" className="relative h-[800px] md:h-auto md:min-h-[700px]">
       {/* Background Image */}
       <div 
         className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: 'url("https://i.imgur.com/Y7GtfwS.jpeg")',
-          backgroundPosition: 'center top'
+          backgroundImage: `url("${window.innerWidth < 768 ? 'https://i.imgur.com/pfnf4hc.jpeg' : 'https://i.imgur.com/4U5ngny.jpeg'}")`,
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover'
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-32 pb-16">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 md:pt-32 pb-12 md:pb-16">
         <div className="grid md:grid-cols-2 gap-8">
           <div className="text-white text-center md:text-right">
             <div className="md:absolute md:right-[50%] md:translate-x-[-2rem] md:top-[200px]">
